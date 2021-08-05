87f3ce92059ed7644af2f3fb6a8ffc179e31c2e7
JMallone <michels@alunos.utfpr.edu.br>
2021-08-05 11:17:00 -0300

    Ajustando algumas funções

--- Laboratorio_07/ex2/noel.c
+++ Laboratorio_07/ex2/noel.c
@@ -1,15 +1,8 @@
 /*
  *  Autor:              Juliano Cesar Petini & Michel Gomes de Souza
- *  Data de criação:    04/08/2021
+ *  Data de criação:    05/08/2021
  *  Descrição:           
- *          Considere um arquivo que é acessado para leitura e escrita por diferentes threads 
- *          de forma concorrente.Se o arquivo estiver sendo lido, diferentes threads podem fazer 
- *          o acesso simultaneamente. Se o arquivoestiver sendo escrito, nenhuma outra thread pode 
- *          ler ou escrever no arquivo, isto é, o acesso à escritadeve ser exclusivo. 
- *          Esse padrão de problema é conhecido como o problema dos leitores-escritores.
- *          Implemente uma solução para o problema dos leitores-escritores usando  semáforos. 
- *          Faça uma soluçãoque evite o postergação indefinida das threads escritoras, isto é, 
- *          se chegar uma thread para escrita, elaterá preferência sobre as próximas threads leitoras.
+ *              Problema do papai noel, elfos e renas.
  */
 
 #include<time.h>
@@ -19,48 +12,70 @@
 #include<pthread.h>
 #include<semaphore.h>
 
-int count_renas = 0, count_elfos = 0, flag_noel_elfos = 0, flag_noel_renas = 0;
+int count_renas = 0, count_elfos = 0, flag_noel_elfos = 0, flag_noel_renas = 0,
+    flag_ocupado_rena = 0, flag_ocupado_elfo = 0;
 sem_t mutex_elfos, mutex_renas;
 
  /*
- *  Descrição:  
- *              Controla o fluxo de leitores disponiveis no semaforo;
- *              Controla o uso da região critica
+ *  Descrição:
+ *              Controla o fluxo no mutex de elfos. 
+ *              Controla o acesso a região critica
  */
 void* f_elfos(){
-    srand(time(NULL)); sleep(rand()%5 + 1);
     while(1){
-        if(!flag_noel_elfos && !flag_noel_renas){
-            sem_wait(&mutex_elfos);
-            count_elfos += 1;
-            printf("\nElfo[%d] chegou.", count_elfos);
-            if(count_elfos == 3) { flag_noel_elfos = 1; printf("\nElfos acordaram o Papai Noel!!"); count_elfos = 0; }
-            sleep(1); sem_post(&mutex_elfos);
-        }else{ srand(time(NULL)); sleep(rand()%5 + 2); }
+        if(!flag_noel_elfos && !flag_noel_renas && !flag_ocupado_elfo){
+          flag_ocupado_elfo = 1;
+          sem_wait(&mutex_elfos);
+          count_elfos += 1;
+          printf("\nElfo[%d] chegou.", count_elfos);
+          if (count_elfos == 3) {
+            flag_noel_elfos = 1;
+            printf("\nElfos acordaram o Papai Noel!!");
+            count_elfos = 0;
+            }
+            sleep(1);
+            if(!flag_noel_elfos && !flag_noel_renas) {flag_ocupado_elfo = 0;}
+            sem_post(&mutex_elfos);
+        } else {
+          srand(time(NULL));
+          sleep(rand() % 10 + 4);
+        }
     }
 }
 
  /*
- *  Descrição:  
- *              Solicita o acesso a região critica atraves da flag_escritor.
+ *  Descrição:
+ *              Controla o fluxo no mutex das renas. 
+ *              Controla o acesso a região critica
  */
 void* f_renas(){
     srand(time(NULL)); sleep(rand()%10 + 2);
     while(1){
-        if(!flag_noel_renas){
+        if(!flag_noel_renas && !flag_ocupado_rena){
+            flag_ocupado_rena = 1;
             sem_wait(&mutex_renas);
             count_renas += 1;
             if(count_renas < 9) printf("\nRena[%d] chegou e foi para o estabulo.", count_renas);
             else printf("\nUltima rena chegou e esta no frio esperando o Papai Noel!!");
-            if(count_renas == 9) { flag_noel_renas = 1; printf("\nTodas as renas chegaram para o natal e acordaram o Papai Noel!!"); count_renas = 0; }
-            sleep(1); sem_post(&mutex_renas);
-        }else{ srand(time(NULL)); sleep(rand()%5 + 2); }
+            if (count_renas == 9) {
+              flag_noel_renas = 1;
+              printf("\nTodas as renas chegaram para o natal e acordaram o Papai Noel!!");
+              count_renas = 0;
+              count_elfos = 0;
+            }
+            sleep(1);
+            if (!flag_noel_renas) { flag_ocupado_rena = 0;}
+            sem_post(&mutex_renas);
+        } else {
+          srand(time(NULL));
+          sleep(rand() % 10 + 5);
+        }
     }
 }
 
  /*
  *  Descrição:  
- *              Solicita o acesso a região critica atraves da flag_escritor.
+ *             Gerencia variaveis de controle para as threads de renas e elfos.
  */
 void* f_noel(){
     while(1){
@@ -69,15 +84,18 @@ void* f_noel(){
             printf("\n|    Papai Noel sai com as renans para fazer as entregas do natal    |");
             printf("\n+--------------------------------------------------------------------+");
             printf("\nPapai Noel volta a dormir");
-            sleep(5);
+            sleep(1);
             flag_noel_renas = 0;
+            flag_ocupado_rena = 0;
+            flag_ocupado_elfo = 0;
         }else if(flag_noel_elfos){
             printf("\n+---------------------------------------------------------+");
             printf("\n|    Papai Noel resolvendo as dificuldades dos 3 Elfos    |");
             printf("\n+---------------------------------------------------------+");
             printf("\nPapai Noel volta a dormir");
-            sleep(2);
+            sleep(1);
             flag_noel_elfos = 0;
+            flag_ocupado_elfo = 0;
         }else{ sleep(1); }
     }
 }
